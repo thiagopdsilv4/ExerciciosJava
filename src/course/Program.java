b5158766f13152d27e74a7d97ca0af716f48ba94
Thiago Pinheiro <thiagopdsilva1@gmail.com>
2022-05-07 18:12:44 -0300

    Triangulo com POO

--- src/course/Program.java
+++ src/course/Program.java
@@ -1,42 +1,47 @@
 package course;
 
+import java.util.Locale;
 import java.util.Scanner;
+
+import entities.Triangle;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-			Scanner sc = new Scanner(System.in);
-			
-			System.out.println("Digite tres numeros: ");
-			
-			int a = sc.nextInt();
-			int b = sc.nextInt();
-			int c = sc.nextInt();
-			
-			int higher = max(a,b,c);		
-			
-			showResult(higher);
-			
-			sc.close();	
-	}
-	
-	public static int max(int x, int y, int z) {
-		int aux;
-		if(x > y && x > z) {
-			aux = x;
-		}
-		else if(y > z) {
-			aux = y;
+		Locale.setDefault(Locale.US);
+
+		Scanner sc = new Scanner(System.in);
+		
+		Triangle x, y;
+		x = new Triangle();
+		y = new Triangle();
+
+		System.out.println("Digite as medidas do triangulo X: ");
+		x.a = sc.nextDouble();
+		x.b = sc.nextDouble();
+		x.c = sc.nextDouble();
+
+		System.out.println("Digite as medidas do triangulo y: ");
+		y.a = sc.nextDouble();
+		y.b = sc.nextDouble();
+		y.c = sc.nextDouble();		
+				
+		double areaX = x.area();
+		double areaY = y.area();
+		
+		System.out.printf("Triangulo X: %.4f%n", areaX);
+		System.out.printf("Triangulo Y: %4f%n", areaY);
+		
+		if(areaX > areaY) {
+			System.out.println("Area maior: X");
 		}
 		else {
-			aux = z;
+			System.out.println("Area maior: Y");
 		}
-		return aux;
-	}
-	
-	public  static void showResult(int value ) {
-		System.out.println("Higher =  " + value);
+		
+		
+		sc.close();
 	}
 
 }
